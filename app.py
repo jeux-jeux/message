f3b4bf8579d81b939ce64778465c7d7cca648893
jeux-jeux <danielersen@icloud.com>
2025-10-26 19:40:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -91,5 +91,3 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
     # Hôte 0.0.0.0 pour permettre l'accès depuis l'extérieur (sur un service comme Render)
     app.run(host='0.0.0.0', port=port)
-
-
